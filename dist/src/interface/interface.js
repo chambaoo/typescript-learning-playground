a6d012ddf291075b052dc13c1a808c1888b442d3
Yuko Oba <chambao752@gmail.com>
2024-08-06 22:57:29 +0900

    add: function type alias / optional parameter

--- dist/src/interface/interface.js
+++ dist/src/interface/interface.js
@@ -23,10 +23,32 @@ class Developer {
     }
 }
 const developer = new Developer('Meta', 33, 13);
-// -------------------------------
-// function type alias
-// -------------------------------
-// -------------------------------
-// optional parameter
-// -------------------------------
+let addFunc;
+addFunc = (n1, n2) => {
+    return n1 + n2;
+};
+class MangaExhibitionTicket {
+    id;
+    name;
+    place;
+    isUsed;
+    // デフォルト引数にする
+    // 省略可能な引数は、必須の引数の後ろに書かないと、コンパイルエラーになる。
+    constructor(name, place, isUsed = false) {
+        this.name = name;
+        this.place = place;
+        this.isUsed = isUsed;
+    }
+    use = () => {
+        if (this.isUsed)
+            throw new Error('This ticket is already used.');
+        this.isUsed = true;
+    };
+}
+// const meikyuTansaku = new MangaExhibitionTicket('迷宮探索', '京都');
+// undefined を明示的に引数に渡した場合も、デフォルトパラメータが適用される。（結果は↑の行と同じになる。）
+const meikyuTansaku = new MangaExhibitionTicket('迷宮探索', '京都', undefined);
+console.log(meikyuTansaku);
+meikyuTansaku.use();
+console.log(meikyuTansaku);
 //# sourceMappingURL=interface.js.map
--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -65,10 +65,76 @@ interface Server extends NameTagType, NameTagInterface {
 // -------------------------------
 // function type alias
 // -------------------------------
+// 関数の型を type を使って定義する
+// type addFuncType = (num1: number, num2: number) => number;
+// let addFunc: addFuncType;
 
+// addFunc = (n1: number, n2: number) => {
+//     return n1 + n2;
+// }
+
+// interface でも定義できる（関数もオブジェクトであり、interface を使用可能）
+// オブジェクトと混同するので、以下の書き方はあまり推奨はしない。
+interface addFuncInterface {
+    // method 名を書かない（無名関数のような書き方）
+    // call signature
+    (num1: number, num2: number): number;
+
+    // constructor signature
+    // new キーワードを付けると、コンストラクタ関数となり、オブジェクトのコンストラクタとなる。
+    // 戻り値は関数の戻り値。この場合は、numberとなる。
+    // new(num1: number, num2: number): number;
+}
+let addFunc: addFuncInterface;
+
+addFunc = (n1: number, n2: number) => {
+    return n1 + n2;
+}
 
 
 // -------------------------------
 // optional parameter
 // -------------------------------
+
+type Ticket1 = {
+    name: string;
+    isUsed: boolean;
+    // プロパティそのものが、存在しないことを許容する場合は、「?」を使用する
+    expired?: Date;
+    // プロパティは必須だが、値がundefinedを許容する場合は、Unionを使用する
+    id: number | undefined;
+    use?(): void;
+}
+
+interface ExhibitionTicket1 extends Ticket1 {
+    place: string;
+}
+
+class MangaExhibitionTicket implements ExhibitionTicket1 {
+    id: number | undefined;
+    name: string;
+    place: string;
+    isUsed: boolean;
+    
+    // デフォルト引数にする
+    // 省略可能な引数は、必須の引数の後ろに書かないと、コンパイルエラーになる。
+    constructor(name: string, place: string, isUsed: boolean = false) {
+        this.name = name;
+        this.place = place;
+        this.isUsed = isUsed;
+    }
+
+    use = () => {
+        if (this.isUsed) throw new Error('This ticket is already used.');
+        this.isUsed = true;
+    }
+}
+
+// const meikyuTansaku = new MangaExhibitionTicket('迷宮探索', '京都');
+// undefined を明示的に引数に渡した場合も、デフォルトパラメータが適用される。（結果は↑の行と同じになる。）
+const meikyuTansaku = new MangaExhibitionTicket('迷宮探索', '京都', undefined);
+
+console.log(meikyuTansaku);
+meikyuTansaku.use();
+console.log(meikyuTansaku);
 
