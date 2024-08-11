822e81b806787e13e1a2b8f8a243aa590294e0a6
chambaoo <105230591+chambaoo@users.noreply.github.com>
2024-08-11 06:36:31 +0000

    add: non-null assertion

--- src/advance/typeAssertion.ts
+++ src/advance/typeAssertion.ts
@@ -3,11 +3,11 @@
 // e.g.
 // <input id='input'>
 
-// const input = document.getElementById('input'); // HTMLElement | null
+// const input = document.getElementById('input'); // const input: HTMLElement | null
 // input.value = 'input value insert'; // プロパティ 'value' は型 'HTMLElement' に存在しません
 
 
-// 型あさーしょんの書き方は２パターンある。
+// 型アサーションの書き方は２パターンある。
 // 1.
 const input1 = <HTMLInputElement>document.getElementById('input');
 input1.value = 'input value insert';
@@ -18,3 +18,8 @@ input2.value = 'input value insert';
 // 1行で書くと、、、
 (<HTMLInputElement>document.getElementById('input')).value = 'input value insert';
 (document.getElementById('input') as HTMLInputElement).value = 'input value insert';
+
+
+// Non-null assertion operator
+// null or undefined ではないことを宣言する
+const input = document.getElementById('input')!; // const input: HTMLElement
